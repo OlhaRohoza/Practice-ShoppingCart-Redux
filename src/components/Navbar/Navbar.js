bbb8a9908720fe247b9fc1183d291f724671d13e
OlhaRohoza <olha.rohoza@gmail.com>
2022-08-16 12:49:28 +0200

    redux store and compose with devTools

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,17 +4,7 @@ import styles from "./Navbar.module.css";
 
 import { connect } from "react-redux";
 
-const Navbar = ({ cart }) => {
-  const [cartCount, setCartCount] = useState(0);
-
-  useEffect(() => {
-    let count = 0;
-    cart.forEach((item) => {
-      count += item.qty;
-    });
-
-    setCartCount(count);
-  }, [cart, cartCount]);
+const Navbar = () => {
 
   return (
     <div className={styles.navbar}>
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 // Redux
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
